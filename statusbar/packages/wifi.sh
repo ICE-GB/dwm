9cc7c12a16f7416d6c60eb07973b886b5c777e69
awnlzw <awnlzw@126.com>
2023-02-10 18:23:32 +0800

    网络管理器

--- statusbar/packages/wifi.sh
+++ statusbar/packages/wifi.sh
@@ -42,11 +42,7 @@ notify() {
 }
 
 call_nm() {
-  pid1=$(ps aux | grep 'st -t statusutil' | grep -v grep | awk '{print $2}')
-  pid2=$(ps aux | grep 'st -t statusutil_nm' | grep -v grep | awk '{print $2}')
-  mx=$(xdotool getmouselocation --shell | grep X= | sed 's/X=//')
-  my=$(xdotool getmouselocation --shell | grep Y= | sed 's/Y=//')
-  kill $pid1 && kill $pid2 || st -t statusutil_nm -g 60x25+$((mx - 240))+$((my + 20)) -c FGN -C "#222D31@4" -e 'nmtui-connect'
+  kcmshell5 kcm_networkmanagement
 }
 
 click() {

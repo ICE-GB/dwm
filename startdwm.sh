1a065852bbbf2f05ff98254c6e886e5be8293953
awnlzw <awnlzw@126.com>
2023-02-11 12:41:30 +0800

    在不登出和退出程序程序的情况下重启dwm

---
+++ startdwm.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/bash
+
+source ~/.profile
+
+# 在不登出和退出程序程序的情况下重启dwm
+# super ctrl f12 重启
+# pkill dwm 真正地退出
+# https://wiki.archlinuxcn.org/wiki/Dwm#:~:text=%E5%9C%A8%E4%B8%8D%E7%99%BB%E5%87%BA%E5%92%8C%E9%80%80%E5%87%BA%E7%A8%8B%E5%BA%8F%E7%A8%8B%E5%BA%8F%E7%9A%84%E6%83%85%E5%86%B5%E4%B8%8B%E9%87%8D%E5%90%AFdwm
+while true; do
+    # Log stderror to a file 
+    # dwm 2> $DWM/dwm.log
+    # No error logging
+    #dwm >/dev/null 2>&1
+    # log all
+    dwm >>$DWM/dwm.log 2>>$DWM/dwm.log
+done

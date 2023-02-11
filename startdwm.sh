a20d29a441ba8259b06649b488c97cf71cc97f54
awnlzw <awnlzw@126.com>
2023-02-11 20:33:30 +0800

    日志文件添加时间后缀

--- startdwm.sh
+++ startdwm.sh
@@ -9,6 +9,7 @@ source ~/.profile
 # pkill dwm 真正地退出
 # https://wiki.archlinuxcn.org/wiki/Dwm#:~:text=%E5%9C%A8%E4%B8%8D%E7%99%BB%E5%87%BA%E5%92%8C%E9%80%80%E5%87%BA%E7%A8%8B%E5%BA%8F%E7%A8%8B%E5%BA%8F%E7%9A%84%E6%83%85%E5%86%B5%E4%B8%8B%E9%87%8D%E5%90%AFdwm
 while true; do
+    log_file=$DWM/logs/dwm-$(date -d "today" +"%Y%m%d%H%M").log
     # Log stderror to a file 
     # dwm 2> $DWM/dwm.log
     # No error logging
@@ -16,5 +17,5 @@ while true; do
     # log all
     # dwm >>$DWM/dwm.log 2>>$DWM/dwm.log
     # log stderror to a file
-    dwm 2>$DWM/dwm.log
+    dwm 2>$log_file
 done

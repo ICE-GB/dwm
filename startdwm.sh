9b5ea5608a368e1a380bc9b1398614ce460b9498
awnlzw <awnlzw@126.com>
2023-03-12 15:00:47 +0800

    dwm启动脚本设置显示器

--- startdwm.sh
+++ startdwm.sh
@@ -3,8 +3,21 @@
 source "$HOME"/.profile
 source "$HOME"/.dwm/.profile
 
-xrandr --dpi 120
-xrandr --output Virtual1 --mode 2560x1440
+if [ "$(uname -n)" == "kubuntu2210" ]; then
+  xrandr --dpi 120
+  xrandr --output Virtual1 --mode 2560x1440
+elif [ "$(uname -n)" == "gb-HP-ProBook-455R-G6" ]; then
+  INNER_PORT=$(xrandr | grep -w 'connected' | head -1 | grep -w 'connected' | awk '{print $1}')
+  # 查找已连接的外接接口
+  OUT_PORT_CONNECTED=$(xrandr | grep -v "$INNER_PORT" | grep -w 'connected' | awk '{print $1}')
+  if [ "" = "$OUT_PORT_CONNECTED" ]; then
+    xrandr --output "$INNER_PORT" --mode 1920x1080 --pos 0x0 --scale 1x1 --primary
+  else
+    xrandr --output "$INNER_PORT" --off --output "$OUT_PORT_CONNECTED" --mode 1920x1080 --pos 0x0 --scale 1x1 --primary
+  fi
+elif [ "$(uname -n)" == "nixos-awnlzw" ]; then
+  echo "由lightdm处理"
+fi
 
 
 # "$HOME"/scripts/set_screen.sh two # 设置显示器

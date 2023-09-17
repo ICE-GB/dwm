48319d9a44b2e2028e04981f54ad19421f534a91
awnlzw <awnlzw@126.com>
2023-09-17 19:27:37 +0800

    添加窗口规则

--- config.h
+++ config.h
@@ -57,30 +57,30 @@ static const char *cursor_theme[] = {"Dracula-cursors"};
 
 // 千万不可以修改顺序，只可以在当前位置改颜色，修改顺序就乱了
 static const char *colors[][3]{
-        {white,             black,             gray2},          // SchemeNorm      普通
-        {red2,              gray2,             pink},           // SchemeSel       选中
-        {white,             gray2,             orange},         // SchemeSelGlobal 全局选中
-        {red2,              black,             black},          // SchemeTabSel    选中tag
-        {white,             black,             black},          // SchemeTabNorm   普通tag
-        {red2,              black,             black},          // SchemeUnderline 下划线
-        {green,             black,             black},          // SchemeMode      模式
-        {white,             black,             black},          // SchemeHid       隐藏
-        {pink,              black,             black},          // SchemeSystray   托盘
-        {white,             black,             nullptr},        // SchemeNormTag   普通标签
-        {white,             black,             nullptr},        // SchemeSelTag    选中标签
-        {black,             black,             nullptr},        // SchemeBarEmpty  空白状态栏
-        {red2,              black,             black},          // SchemeOverView  overview
-        {pink,              black,             pink},           // SchemeStatusText状态栏文本
-        {pink,              black,             pink},           // SchemeTag
-        {blue,              black,             black},          // 1-10
-        {red2,              black,             black},
-        {orange,            black,             black},
-        {green,             black,             black},
-        {pink,              black,             black},
-        {green,             black,             black},
-        {green,             black,             black},
-        {green,             black,             black},
-        {green,             black,             black}
+        {white,  black, gray2},          // SchemeNorm      普通
+        {red2,   gray2, pink},           // SchemeSel       选中
+        {white,  gray2, orange},         // SchemeSelGlobal 全局选中
+        {red2,   black, black},          // SchemeTabSel    选中tag
+        {white,  black, black},          // SchemeTabNorm   普通tag
+        {red2,   black, black},          // SchemeUnderline 下划线
+        {green,  black, black},          // SchemeMode      模式
+        {white,  black, black},          // SchemeHid       隐藏
+        {pink,   black, black},          // SchemeSystray   托盘
+        {white,  black, nullptr},        // SchemeNormTag   普通标签
+        {white,  black, nullptr},        // SchemeSelTag    选中标签
+        {black,  black, nullptr},        // SchemeBarEmpty  空白状态栏
+        {red2,   black, black},          // SchemeOverView  overview
+        {pink,   black, pink},           // SchemeStatusText状态栏文本
+        {pink,   black, pink},           // SchemeTag
+        {blue,   black, black},          // 1-10
+        {red2,   black, black},
+        {orange, black, black},
+        {green,  black, black},
+        {pink,   black, black},
+        {green,  black, black},
+        {green,  black, black},
+        {green,  black, black},
+        {green,  black, black}
 };
 static const int tagschemes[] = {
         SchemeTag1, SchemeTag2, SchemeTag3, SchemeTag4, SchemeTag5, SchemeTag6, SchemeTag7, SchemeTag8, SchemeTag9,
@@ -199,35 +199,39 @@ static const char *avoid_repeat_auto_start = "/tmp/dwm_avoid_repeat_auto_start.l
 static const Rule rules[] = {
         /* class       instance      title         tags mask     isfloating  isglobal    isnoborder monitor floatposition */
         /** 优先级高 越在上面优先度越高 */
-        {NULL,         NULL,         "图片查看器", 0,       1, 0, 0, -1, 0}, // qq图片查看器        浮动
-        {NULL,         NULL,         "图片查看",   0,       1, 0, 0, -1, 0}, // 微信图片查看器      浮动
+        {NULL,                NULL,         "图片查看器", 0,       1, 0, 0, -1, 0}, // qq图片查看器        浮动
+        {NULL,                NULL,         "图片查看",   0,       1, 0, 0, -1, 0}, // 微信图片查看器      浮动
 
         /** 普通优先度 */
-        {"obs",        NULL,         NULL,         1 << 5,  0, 0, 0, -1, 0}, // obs        tag -> 
-        {"chrome",     NULL,         NULL,         1 << 6,  0, 0, 0, -1, 0}, // chrome     tag -> 
-        {"Chromium",   NULL,         NULL,         1 << 6,  0, 0, 0, -1, 0}, // Chromium   tag -> 
-        {"music",      NULL,         NULL,         1 << 7,  1, 0, 1, -1, 0}, // music      tag -> 󰎄 浮动、无边框
-        {NULL,         "qq",         NULL,         1 << 9,  0, 0, 1, -1, 0}, // qq         tag -> 󰘅 无边框
-        {NULL,         "wechat.exe", NULL,         1 << 9,  0, 0, 1, -1, 0}, // wechat     tag -> 󰘑 无边框
-        {NULL,         "wxwork.exe", NULL,         1 << 9,  0, 0, 1, -1, 0}, // workwechat tag -> 󰊠 无边框
-        {"Vncviewer",  NULL,         NULL,         0,       1, 0, 1, -1, 2}, // Vncviewer           浮动、无边框 屏幕顶部
-        {"flameshot",  NULL,         NULL,         0,       1, 0, 0, -1, 0}, // 火焰截图            浮动
-        {"scratchpad", "scratchpad", "scratchpad", TAGMASK, 1, 1, 1, -1, 2}, // scratchpad          浮动、全局、无边框 屏幕顶部
-        {"Pcmanfm",    NULL,         NULL,         0,       1, 0, 1, -1, 3}, // pcmanfm             浮动、无边框 右上角
-        {"wemeetapp",  NULL,         NULL,         TAGMASK, 1, 1, 0, -1, 0}, // !!!腾讯会议在切换tag时有诡异bug导致退出 变成global来规避该问题
+        {"code",              NULL,         NULL,         1 << 1,  0, 0, 0, -1, 0}, // vscode     tag -> 󰅬
+        {"jetbrains-clion",   NULL,         NULL,         1 << 1,  0, 0, 0, -1, 0}, // vscode     tag -> 󰅬
+        {"jetbrains-idea",    NULL,         NULL,         1 << 1,  0, 0, 0, -1, 0}, // vscode     tag -> 󰅬
+        {"jetbrains-pycharm", NULL,         NULL,         1 << 1,  0, 0, 0, -1, 0}, // vscode     tag -> 󰅬
+        {"obs",               NULL,         NULL,         1 << 5,  0, 0, 0, -1, 0}, // obs        tag -> 
+        {"chrome",            NULL,         NULL,         1 << 6,  0, 0, 0, -1, 0}, // chrome     tag -> 
+        {"Chromium",          NULL,         NULL,         1 << 6,  0, 0, 0, -1, 0}, // Chromium   tag -> 
+        {"music",             NULL,         NULL,         1 << 7,  1, 0, 1, -1, 0}, // music      tag -> 󰎄 浮动、无边框
+        {NULL,                "qq",         NULL,         1 << 9,  0, 0, 1, -1, 0}, // qq         tag -> 󰘅 无边框
+        {NULL,                "wechat.exe", NULL,         1 << 9,  0, 0, 1, -1, 0}, // wechat     tag -> 󰘑 无边框
+        {NULL,                "wxwork.exe", NULL,         1 << 9,  0, 0, 1, -1, 0}, // workwechat tag -> 󰊠 无边框
+        {"Vncviewer",         NULL,         NULL,         0,       1, 0, 1, -1, 2}, // Vncviewer           浮动、无边框 屏幕顶部
+        {"flameshot",         NULL,         NULL,         0,       1, 0, 0, -1, 0}, // 火焰截图            浮动
+        {"scratchpad",        "scratchpad", "scratchpad", TAGMASK, 1, 1, 1, -1, 2}, // scratchpad          浮动、全局、无边框 屏幕顶部
+        {"Pcmanfm",           NULL,         NULL,         0,       1, 0, 1, -1, 3}, // pcmanfm             浮动、无边框 右上角
+        {"wemeetapp",         NULL,         NULL,         TAGMASK, 1, 1, 0, -1, 0}, // !!!腾讯会议在切换tag时有诡异bug导致退出 变成global来规避该问题
 
         /** 部分特殊class的规则 */
-        {"float",      NULL,         NULL,         0,       1, 0, 0, -1, 0}, // class = float       浮动
-        {"global",     NULL,         NULL,         TAGMASK, 0, 1, 0, -1, 0}, // class = gloabl      全局
-        {"noborder",   NULL,         NULL,         0,       0, 0, 1, -1, 0}, // class = noborder    无边框
-        {"FGN",        NULL,         NULL,         TAGMASK, 1, 1, 1, -1, 0}, // class = FGN         浮动、全局、无边框
-        {"FG",         NULL,         NULL,         TAGMASK, 1, 1, 0, -1, 0}, // class = FG          浮动、全局
-        {"FN",         NULL,         NULL,         0,       1, 0, 1, -1, 0}, // class = FN          浮动、无边框
-        {"GN",         NULL,         NULL,         TAGMASK, 0, 1, 1, -1, 0}, // CLASS = GN          全局、无边框
+        {"float",             NULL,         NULL,         0,       1, 0, 0, -1, 0}, // class = float       浮动
+        {"global",            NULL,         NULL,         TAGMASK, 0, 1, 0, -1, 0}, // class = gloabl      全局
+        {"noborder",          NULL,         NULL,         0,       0, 0, 1, -1, 0}, // class = noborder    无边框
+        {"FGN",               NULL,         NULL,         TAGMASK, 1, 1, 1, -1, 0}, // class = FGN         浮动、全局、无边框
+        {"FG",                NULL,         NULL,         TAGMASK, 1, 1, 0, -1, 0}, // class = FG          浮动、全局
+        {"FN",                NULL,         NULL,         0,       1, 0, 1, -1, 0}, // class = FN          浮动、无边框
+        {"GN",                NULL,         NULL,         TAGMASK, 0, 1, 1, -1, 0}, // CLASS = GN          全局、无边框
 
         /** 优先度低 越在上面优先度越低 */
-        {NULL,         NULL,         "crx_",       0,       1, 0, 0, -1, 0}, // 错误载入时 会有crx_ 浮动
-        {NULL,         NULL,         "broken",     0,       1, 0, 0, -1, 0}, // 错误载入时 会有broken 浮动
+        {NULL,                NULL,         "crx_",       0,       1, 0, 0, -1, 0}, // 错误载入时 会有crx_ 浮动
+        {NULL,                NULL,         "broken",     0,       1, 0, 0, -1, 0}, // 错误载入时 会有broken 浮动
 };
 
 
